4b1effe27c858a0f98af2dbb28740bc4bf3c76b7
Julia Anne <julia.souza@ufrj.br>
2015-12-28 15:16:49 -0200

    # Alterações e melhorias

--- CacheLRU.c
+++ CacheLRU.c
@@ -1,73 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define loadFactor 0.75
+#define initialSize 16
 
-typedef struct {
+typedef struct _P{
     char nome[40];
-    long int CPF;
+    int idade;
 } PERSON;
 
-typedef struct {
-    char key[40];
-    int *next;
+typedef struct _E{
+    long long int key; //CPF
+    struct _E *prev;
+    struct _E *next;
     PERSON person;
 } ENTRY;
 
-typedef struct {
-    int *first;
-    ENTRY entry;
-    int *last;
+typedef struct _LL{
+    ENTRY *first;
+    ENTRY *last;
 } LINKEDLIST;
 
-typedef struct {
-    LINKEDLIST buckets;
+typedef struct _HM{
+    LINKEDLIST buckets[initialSize];
     int bucketsCount;
     int elementsCount;
-    double currentLoadFactor;
+    float currentLoadFactor;
 } HASHMAP;
 
-void initHashMap(HASHMAP *hashMap){
-    LINKEDLIST *buckets;
-
-    buckets = malloc(sizeof(LINKEDLIST));
-
-    hashMap->buckets= *(LINKEDLIST *) buckets;
-    hashMap->bucketsCount=1;
-    hashMap->elementsCount=0;
+void *initHashMap(HASHMAP *hashMap){
+    hashMap->bucketsCount=initialSize;
+    hashMap->elementsCount=1;
     hashMap->currentLoadFactor=hashMap->elementsCount/hashMap->bucketsCount;
 
-    free(buckets);
+    return hashMap;
 }
 
+int hashCode(long long int key){
+    int code = key % initialSize;
+    //printf("%d\n", code);
+    return code;
+}
 
-/*void *put(HASHMAP *hashMap, char key[40], long int value){
-    //ToDo: Test if Contains(); Se sim, retorna; Se não, insere.
+int contains(HASHMAP *hashMap, long long int key){
+    int code = hashCode(key);
+    ENTRY *current = malloc(sizeof(ENTRY));
+    current = hashMap->buckets[code].first;
 
-    //ToDo: Colocar na primeira posição,
-    PERSON person;
-    ENTRY entry;
-    hashMap->buckets.entry
+    if(current==NULL) return 0;
 
-    person.nome = key[40];
-    person.CPF = value;
+    while(1){
+        if(current->key==key){
+            return 1;
+        }
+        if(current->next==NULL) break;
+        current = current->next;
+    }
 
-    //ToDo: Inseriu! Ultrapassou valor de rehash? Rehash; Retorna;
-    return hashMap;
-}*/
+    return 0;
+}
 
+int put(HASHMAP *hashMap, long long int key, PERSON person){
+    int code = hashCode(key);
+
+    ENTRY *entry = malloc(sizeof(ENTRY));
+    entry->key=key;
+    entry->next=NULL;
+    entry->person=person;
+    hashMap->buckets[code].first=entry;
+    hashMap->buckets[code].last=NULL;
+
+    return 1;
+}
 
 int main(int argc, char *argv[]) {
-    HASHMAP *hashMap;
-
-    hashMap = malloc(sizeof(HASHMAP));
+    HASHMAP *hashMap = malloc(sizeof(HASHMAP));
     initHashMap(hashMap);
+    printf("-- HashMap Inicializado --\n");
 
-    printf("hashMap.bucketsCount: %d\n", hashMap->bucketsCount);
-    printf("hashMap.elementsCount: %d\n", hashMap->elementsCount);
-    printf("hashMap.loadFactor: %f\n", hashMap->currentLoadFactor);
+    long long int key = 20677346522;
+    PERSON person;
+    strcpy(person.nome, "Julia Anne");
+    person.idade=22;
+    if(put(hashMap, key, person)){
+        printf("-- Person %s Inserido --\n", person.nome);
+    } else printf("-- ERRO DE INSERÇÃO --\n");
 
-    printf("-- HashMap Inicializado\n");
+    printf("%d\n", contains(hashMap,key));
+
+    /*
+    printf("%d\n",hashCode(20677346522));   10
+    printf("%d\n",hashCode(88272078481));   1
+    printf("%d\n",hashCode(53604310598));   6
+    printf("%d\n",hashCode(85779623341));   13
+    printf("%d\n",hashCode(19245264173));   13*/
 
     free(hashMap);
     return 0;
