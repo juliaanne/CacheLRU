bdf343714cd7112978af92f052e71c6a8a5e9eaa
Julia Anne <julia.souza@ufrj.br>
2015-12-28 22:22:30 -0200

    # Finalizando ultimos detalhes

--- CacheLRU.c
+++ CacheLRU.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define loadFactor 0.75
-#define initialSize 16
+#define initialSize 8
 
 typedef struct _P{
     char nome[40];
@@ -26,13 +26,13 @@ typedef struct _HM{
     LINKEDLIST buckets[initialSize];
     int bucketsCount;
     int elementsCount;
-    float currentLoadFactor;
+    double currentLoadFactor;
 } HASHMAP;
 
 void *initHashMap(HASHMAP *hashMap){
     hashMap->bucketsCount=initialSize;
     hashMap->elementsCount=0;
-    hashMap->currentLoadFactor=hashMap->elementsCount/hashMap->bucketsCount;
+    hashMap->currentLoadFactor=(float) hashMap->elementsCount/(float) hashMap->bucketsCount;
 
     return hashMap;
 }
@@ -48,14 +48,18 @@ int contains(HASHMAP *hashMap, long long int key){
     ENTRY *current = malloc(sizeof(ENTRY));
     current = hashMap->buckets[code].first;
 
-    if(current==NULL) return 0;
+    if(current==NULL) return 0; // Bucket vazio
+    if(current->key==key) return 1;
 
     while(1){
         if(current->key==key){
             // Se contém, devo atualizar para mais recente
+            if(current->next==NULL) { hashMap->buckets[code].last = current->prev; }
+            else { current->next->prev=current->prev; }
+            current->prev->next=current->next;
+            current->prev=NULL;
             hashMap->buckets[code].first->prev=current;
             current->next=hashMap->buckets[code].first;
-            current->prev=NULL;
             hashMap->buckets[code].first=current;
             return 1;
         }
@@ -66,7 +70,6 @@ int contains(HASHMAP *hashMap, long long int key){
     return 0;
 }
 
-
 int put(HASHMAP *hashMap, long long int key, PERSON person){
     ENTRY *entry = malloc(sizeof(ENTRY));
     int code = hashCode(key);
@@ -76,18 +79,32 @@ int put(HASHMAP *hashMap, long long int key, PERSON person){
         return 0;
     }
 
-    if(((hashMap->elementsCount+1)/hashMap->bucketsCount) > loadFactor ){
+    // Atualizando valores
+    hashMap->elementsCount+=1;
+
+    if(hashMap->buckets[code].first != NULL && (float) hashMap->elementsCount/(float) hashMap->bucketsCount > loadFactor){
         // NÃO TEM MAIS ESPAÇO, PRECISO REMOVER ANTES DE INSERIR
-        hashMap->buckets[code].last=hashMap->buckets[code].last->prev;
-        hashMap->buckets[code].last->next->prev=NULL;
-        free(hashMap->buckets[code].last->next);
-        hashMap->buckets[code].last->next=NULL;
+        printf("\n(!!!) Será removido %s para que %s possa ser inserido\n\n", hashMap->buckets[code].last->person.nome, person.nome);
+        if(hashMap->buckets[code].last->prev==NULL){
+            // O último é o primeiro
+            free(hashMap->buckets[code].last);
+            hashMap->buckets[code].last=NULL;
+            hashMap->buckets[code].first=NULL;
+        }else{
+            // Existe uma ou mais entries
+            hashMap->buckets[code].last = hashMap->buckets[code].last->prev;
+            hashMap->buckets[code].last->next->prev=NULL;
+            free(hashMap->buckets[code].last->next);
+            hashMap->buckets[code].last->next=NULL;
+        }
+        hashMap->elementsCount--;
     }
 
     // INSERINDO
     entry->key=key;
     entry->person=person;
     entry->prev=NULL;
+
     if(hashMap->buckets[code].first == NULL){
         // Bucket VAZIO
         entry->next=NULL;
@@ -100,59 +117,80 @@ int put(HASHMAP *hashMap, long long int key, PERSON person){
         hashMap->buckets[code].first=entry;
     }
 
-    // Atualizando valores
-    hashMap->elementsCount+=1;
-    hashMap->currentLoadFactor=hashMap->elementsCount/hashMap->bucketsCount;
+    hashMap->currentLoadFactor= (float) hashMap->elementsCount/(float) hashMap->bucketsCount;
+    if(hashMap->currentLoadFactor>loadFactor)
+        printf("\n(!!!) ESTOUROU LOADFACTOR %f\n\n", hashMap->currentLoadFactor);
 
     return 1;
 }
 
 void populando(HASHMAP *hashMap){
-    long long int key1 = 20677346522; // 10
-    long long int key2 = 88272078481; // 1
-    long long int key3 = 53604310598; // 6
-    long long int key4 = 85779623341; // 13
-    long long int key5 = 19245264173; // 13
+    long long int key1 = 20677346522; //2
+    long long int key2 = 88272078481; //1
+    long long int key3 = 53604310598; //6
+    long long int key4 = 85779623341; //5
+    long long int key5 = 19245264173; //5
+    long long int key6 = 53687972224; //0
+    long long int key7 = 82725890870; //6 - Não aumentará o loadFactor, pois removerá a key3
+    long long int key8 = 75661233191; //7 - Aumentará o loadFactor mas não removerá
+
 
     PERSON person1;
     strcpy(person1.nome, "Julia Anne");
     person1.idade=22;
-    put(hashMap, key1, person1)? printf("-- Person %s Inserido --\n", person1.nome): printf("-- ERRO DE INSERÇÃO --\n");
+    put(hashMap, key1, person1)? printf("-- %s Inserido\n", person1.nome): printf("-- ERRO DE INSERÇÃO\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
 
     PERSON person2;
     strcpy(person2.nome, "Eliza Alves");
     person2.idade=15;
-    put(hashMap, key2, person2)? printf("-- Person %s Inserido --\n", person2.nome): printf("-- ERRO DE INSERÇÃO --\n");
+    put(hashMap, key2, person2)? printf("-- %s Inserido\n", person2.nome): printf("-- ERRO DE INSERÇÃO\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
 
     PERSON person3;
     strcpy(person3.nome, "Ed de Souza");
     person3.idade=55;
-    put(hashMap, key3, person3)? printf("-- Person %s Inserido --\n", person3.nome): printf("-- ERRO DE INSERÇÃO --\n");
+    put(hashMap, key3, person3)? printf("-- %s Inserido\n", person3.nome): printf("-- ERRO DE INSERÇÃO\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
 
     PERSON person4;
     strcpy(person4.nome, "Afonso Carvalho");
     person4.idade=22;
-    put(hashMap, key4, person4)? printf("-- Person %s Inserido --\n", person4.nome): printf("-- ERRO DE INSERÇÃO --\n");
+    put(hashMap, key4, person4)? printf("-- %s Inserido\n", person4.nome): printf("-- ERRO DE INSERÇÃO\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
 
     PERSON person5;
     strcpy(person5.nome, "Carina Camacchi");
     person5.idade=27;
-    put(hashMap, key5, person5)? printf("-- Person %s Inserido --\n", person5.nome): printf("-- ERRO DE INSERÇÃO --\n");
+    put(hashMap, key5, person5)? printf("-- %s Inserido\n", person5.nome): printf("-- ERRO DE INSERÇÃO\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
 
+    PERSON person6;
+    strcpy(person6.nome, "João Carvalho");
+    person6.idade=46;
+    put(hashMap, key6, person6)? printf("-- %s Inserido --\n", person6.nome): printf("-- ERRO DE INSERÇÃO\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
+
+    PERSON person7;
+    strcpy(person7.nome, "Amanda Medeiros");
+    person7.idade=48;
+    put(hashMap, key7, person7)? printf("-- %s Inserido --\n", person7.nome): printf("-- ERRO DE INSERÇÃO --\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
+
+    PERSON person8;
+    strcpy(person8.nome, "Alice Oliveira");
+    person8.idade=18;
+    put(hashMap, key8, person8)? printf("-- Person %s Inserido --\n", person8.nome): printf("-- ERRO DE INSERÇÃO --\n");
+    //printf("%f\n", hashMap->currentLoadFactor);
 }
-
 
 int main(int argc, char *argv[]) {
     HASHMAP *hashMap = malloc(sizeof(HASHMAP));
     initHashMap(hashMap);
-    printf("-- HashMap Inicializado --\n");
+    //printf("-- HashMap Inicializado com LOADFACTOR = %f --\n", hashMap->currentLoadFactor);
 
     populando(hashMap);
-    printf("-- HashMap Populado --\n");
-
-    printf("Antes de consultar\n%lld\n", hashMap->buckets[13].first->key);
-    printf("%d\n", contains(hashMap,85779623341));
-    printf("Após consultar\n%lld\n", hashMap->buckets[13].first->key);
+    //printf("-- HashMap Populado --\n");
 
     free(hashMap);
     return 0;
